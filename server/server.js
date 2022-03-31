84a356db6116c87fd2a75dda6f6d8c8b13e4db98
JosephJamesCoop <cooper_3869@live.com>
2022-03-31 09:43:44 -0700

    finished backend APIs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -9,12 +10,19 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const {ApolloServerPluginLandingPageGraphQLPlayground} = require("apollo-server-core");
+
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
-    // context: authMiddleware 
+    context: authMiddleware,
+    plugins: [
+
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+  
+    ],
   });
 
   // Start the Apollo server
